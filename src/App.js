b64881ef49a2d0aa4642cc465825dd202c968ffc
Alfiado <alfiado02@gmail.com>
2020-02-27 00:54:06 +0200

    defining custom elements

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useState, useMemo, useCallback } from 'react';
+import { createEditor, Transforms, Editor } from 'slate';
+import { Slate, Editable, withReact } from 'slate-react';
+
 import './App.css';
 
 function App() {
+  // Create a Slate editor object that won't change across renders.
+  const editor = useMemo(() => withReact(createEditor()), []);
+
+  // kepp track of state for the value of editor
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'a line of text in a paragraph' }],
+    },
+  ]);
+
+  // Define a rendering function based on the element passed to `props`. We use
+  // `useCallback` here to memoize the function for subsequent renders.
+  const renderElement = useCallback(props => {
+    const { element } = props;
+    switch (element.type) {
+      case 'code':
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
+
+  // render the slate context
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Slate editor={editor} value={value} onChange={value2 => setValue(value2)}>
+      <Editable
+        renderElement={renderElement}
+        onKeyDown={event => {
+          if (event.key === '1' && event.ctrlKey) {
+            event.preventDefault();
+            // otherwise, set the currently selected blocks type to "code"
+            const [match] = Editor.nodes(editor, {
+              match: n => n.type === 'code',
+            });
+            Transforms.setNodes(
+              editor,
+              { type: match ? 'paragraph' : 'code' },
+              { match: n => Editor.isBlock(editor, n) }
+            );
+          }
+        }}
+      />
+    </Slate>
   );
 }
+
+const CodeElement = ({ attributes, children }) => {
+  return (
+    <pre {...attributes}>
+      <code>{children}</code>
+    </pre>
+  );
+};
+
+const DefaultElement = ({ attributes, children }) => (
+  <p {...attributes}>{children}</p>
+);
 
 export default App;

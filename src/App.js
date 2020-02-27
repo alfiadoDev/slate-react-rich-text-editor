113f06479608c33e23170a38c6c4fe21a1a29504
Alfiado <alfiado02@gmail.com>
2020-02-27 02:16:59 +0200

    applying custom formatting

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { useState, useMemo, useCallback } from 'react';
 import { createEditor, Transforms, Editor } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react';
+import PropTypes from 'prop-types';
 
 import './App.css';
 
@@ -29,23 +30,49 @@ function App() {
     }
   }, []);
 
+  const renderLeaf = useCallback(props => {
+    return <Leaf {...props} />;
+  });
+
   // render the slate context
   return (
     <Slate editor={editor} value={value} onChange={value2 => setValue(value2)}>
       <Editable
         renderElement={renderElement}
+        renderLeaf={renderLeaf}
         onKeyDown={event => {
-          if (event.key === '1' && event.ctrlKey) {
-            event.preventDefault();
-            // otherwise, set the currently selected blocks type to "code"
-            const [match] = Editor.nodes(editor, {
-              match: n => n.type === 'code',
-            });
-            Transforms.setNodes(
-              editor,
-              { type: match ? 'paragraph' : 'code' },
-              { match: n => Editor.isBlock(editor, n) }
-            );
+          if (!event.ctrlKey) return;
+
+          switch (event.key) {
+            case '1': {
+              event.preventDefault();
+              // otherwise, set the currently selected blocks type to "code"
+              const [match] = Editor.nodes(editor, {
+                match: n => n.type === 'code',
+              });
+              Transforms.setNodes(
+                editor,
+                { type: match ? 'paragraph' : 'code' },
+                { match: n => Editor.isBlock(editor, n) }
+              );
+
+              break;
+            }
+
+            case 'b': {
+              event.preventDefault();
+              console.log(Text);
+              Transforms.setNodes(
+                editor,
+                { bold: true },
+                // Apply it to text nodes, and split the text node up if the
+                // selection is overlapping only part of it.
+                { match: n => new Text(n), split: true }
+              );
+              break;
+            }
+
+            default:
           }
         }}
       />
@@ -65,4 +92,32 @@ const DefaultElement = ({ attributes, children }) => (
   <p {...attributes}>{children}</p>
 );
 
+// define a react component to render leaves with bold text
+const Leaf = ({ attributes, children, leaf }) => (
+  <span {...attributes} style={{ fontWeight: leaf.bold ? 'bold' : 'normal' }}>
+    {children}
+  </span>
+);
+
+/* App.propTypes = {
+  element: PropTypes.shape({
+    type: PropTypes.string,
+  }).isRequired,
+};
+DefaultElement.propTypes = {
+  attributes: PropTypes.element.isRequired,
+  children: PropTypes.element.isRequired,
+};
+Leaf.propTypes = {
+  attributes: PropTypes.element.isRequired,
+  children: PropTypes.element.isRequired,
+  leaf: PropTypes.shape({
+    bold: PropTypes.string,
+  }).isRequired,
+};
+CodeElement.propTypes = {
+  attributes: PropTypes.element.isRequired,
+  children: PropTypes.element.isRequired,
+};
+ */
 export default App;

6fd843e68ac8890ec0599b4fba3aeb2ddf2c283d
Alfiado <alfiado02@gmail.com>
2020-02-27 15:08:33 +0200

    executing commands and toolbar buttons

--- src/App.js
+++ src/App.js
@@ -2,9 +2,50 @@
 import React, { useState, useMemo, useCallback } from 'react';
 import { createEditor, Transforms, Editor } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import Icon from 'react-icons-kit';
+import { bold } from 'react-icons-kit/feather/bold';
+import { italic } from 'react-icons-kit/feather/italic';
+import { code } from 'react-icons-kit/feather/code';
+import { list } from 'react-icons-kit/feather/list';
+import { underline } from 'react-icons-kit/feather/underline';
+
+import FormatToolbar from './components/FormatToolbar';
 
 import './App.css';
+
+// define a custom set of helpers
+const CustomEditor = {
+  idBoldMarkActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.bold === true,
+      universal: true,
+    });
+    return !!match;
+  },
+  isCodeBlockActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.type === 'code',
+    });
+    return !!match;
+  },
+  toggleBoldMark(editor) {
+    const isActive = CustomEditor.idBoldMarkActive(editor);
+    Transforms.setNodes(
+      editor,
+      { bold: isActive ? null : true },
+      { match: n => new Text(n), split: true }
+    );
+  },
+  toggleCodeBlock(editor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor);
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? null : 'code' },
+      { match: n => Editor.isBlock(editor, n) }
+    );
+  },
+};
 
 function App() {
   // Create a Slate editor object that won't change across renders.
@@ -36,47 +77,92 @@ function App() {
 
   // render the slate context
   return (
-    <Slate editor={editor} value={value} onChange={value2 => setValue(value2)}>
-      <Editable
-        renderElement={renderElement}
-        renderLeaf={renderLeaf}
-        onKeyDown={event => {
-          if (!event.ctrlKey) return;
-
-          switch (event.key) {
-            case '1': {
+    <div className="App">
+      <Slate
+        editor={editor}
+        value={value}
+        onChange={value2 => setValue(value2)}
+      >
+        <FormatToolbar>
+          <button
+            type="button"
+            className="tooltip-icon-button"
+            onPointerDown={event => {
               event.preventDefault();
-              // otherwise, set the currently selected blocks type to "code"
-              const [match] = Editor.nodes(editor, {
-                match: n => n.type === 'code',
-              });
-              Transforms.setNodes(
-                editor,
-                { type: match ? 'paragraph' : 'code' },
-                { match: n => Editor.isBlock(editor, n) }
-              );
-
-              break;
-            }
-
-            case 'b': {
+              CustomEditor.toggleBoldMark(editor);
+            }}
+          >
+            <Icon icon={bold} />
+          </button>
+          <button 
+            type="button" 
+            className="tooltip-icon-button"
+            onPointerDown={event => {
               event.preventDefault();
-              console.log(Text);
-              Transforms.setNodes(
-                editor,
-                { bold: true },
-                // Apply it to text nodes, and split the text node up if the
-                // selection is overlapping only part of it.
-                { match: n => new Text(n), split: true }
-              );
-              break;
-            }
+              // CustomEditor.toggleItalicMark(editor);
+            }}
+          >
+            <Icon icon={italic} />
+          </button>
+          <button 
+            type="button" 
+            className="tooltip-icon-button"
+            onPointerDown={event => {
+              event.preventDefault();
+              // CustomEditor.toggleItalicMark(editor);
+            }}
+          >
+            <Icon icon={code} />
+          </button>
+          <button 
+            type="button" 
+            className="tooltip-icon-button"
+            onPointerDown={event => {
+              event.preventDefault();
+              // CustomEditor.toggleItalicMark(editor);
+            }}
+          >
+            <Icon icon={list} />
+          </button>
+          <button 
+            type="button" 
+            className="tooltip-icon-button"
+            onPointerDown={event => {
+              event.preventDefault();
+              // CustomEditor.toggleItalicMark(editor);
+            }}
+          >
+            <Icon icon={underline} />
+          </button>
+        </FormatToolbar>
+        <Editable
+          style={{ textAlign: 'left' }}
+          renderElement={renderElement}
+          renderLeaf={renderLeaf}
+          onKeyDown={event => {
+            if (!event.ctrlKey) return;
 
-            default:
-          }
-        }}
-      />
-    </Slate>
+            // Replace the `onKeyDown` logic with our new commands.
+
+            switch (event.key) {
+              case '1': {
+                event.preventDefault();
+                CustomEditor.toggleCodeBlock(editor);
+                break;
+              }
+
+              case 'b': {
+                event.preventDefault();
+                CustomEditor.toggleBoldMark(editor);
+                break;
+              }
+
+              default:
+            }
+          }}
+        />
+      </Slate>
+    </div>
   );
 }
 
---
+++ src/components/FormatToolbar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FormatToolbar = ({ children }) => (
+  <div className="format-toolbar">{children}</div>
+);
+
+export default FormatToolbar;
